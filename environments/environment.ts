b9a27117f99b062e76b8d1d4a57884d0a0245860
Aday González <adaymaster@gmail.com>
2022-09-18 17:12:58 +0100

    feat: environment updated

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
+  newsApiUrl: "https://newsapi.org",
   newsApiKey: "daf4935eb5d14a5a9374af885707d57e",
+  /** Maximum time in minutes for the saved news to be valid. */
+  newsRefreshTimeInMinutes: 60,
 };
 
