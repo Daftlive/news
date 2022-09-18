8367fa25edca951517e223ebaa29e93b14842051
Aday González <adaymaster@gmail.com>
2022-09-18 17:17:34 +0100

    feat: StatusBar && background changed

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, SafeAreaView } from "react-native";
-import Router from "./app/navigation/router";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import Router from "./app/navigations/router";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
       <Router />
     </SafeAreaView>
   );
@@ -12,6 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f2f2f2",
   },
 });
 
